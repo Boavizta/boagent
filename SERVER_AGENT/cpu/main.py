d20512de0aff82c17d50f5909dd654a5fe5b93ae
benjaminlebigot <benjaminlebigot@gmail.com>
2022-03-18 17:12:14 +0100

    use openAPI sdk for the cpu case

--- SERVER_AGENT/cpu/main.py
+++ SERVER_AGENT/cpu/main.py
@@ -2,6 +2,21 @@
 from cpuinfo import get_cpu_info
 from cpuid import *
 import cpuid_native
+import openapi_client
+from openapi_client.model import component_cpu
+import requests
+
+def get_cpu_info():
+
+    cpu_obj = component_cpu.ComponentCPU()
+    cpu_obj.core_units = get_cpu_info()["count"]
+    cpu_obj.manufacturer = cpu_vendor()
+    cpu_obj.family = str(cpu_microarchitecture())
+    print(cpu_obj)
+
+    return cpu_obj.openapi_types()
+
+
 
 def is_set(id, reg_idx, bit):
     regs = cpuid(id)
@@ -11,21 +26,30 @@ def is_set(id, reg_idx, bit):
     else:
         return "--"
 
-print("Vendor ID         : %s" % cpu_vendor())
-print("CPU name          : %s" % cpu_name())
-print("Microarchitecture : %s%s" % cpu_microarchitecture())
-print("Vector instructions supported:")
-print("SSE       : %s" % is_set(1, 3, 25))
-print("SSE2      : %s" % is_set(1, 3, 26))
-print("SSE3      : %s" % is_set(1, 2, 0))
-print("SSSE3     : %s" % is_set(1, 2, 9))
-print("SSE4.1    : %s" % is_set(1, 2, 19))
-print("SSE4.2    : %s" % is_set(1, 2, 20))
-print("SSE4a     : %s" % is_set(0x80000001, 2, 6))
-print("AVX       : %s" % is_set(1, 2, 28))
-print("AVX2      : %s" % is_set(7, 1, 5))
-print("BMI1      : %s" % is_set(7, 1, 3))
-print("BMI2      : %s" % is_set(7, 1, 8))
 
 
-print("{}".format(get_cpu_info()))
+if __name__ == "__main__":
+
+
+    print("Vendor ID         : %s" % cpu_vendor())
+    print("CPU name          : %s" % cpu_name())
+    print("Microarchitecture : %s%s" % cpu_microarchitecture())
+    print("Vector instructions supported:")
+    print("SSE       : %s" % is_set(1, 3, 25))
+    print("SSE2      : %s" % is_set(1, 3, 26))
+    print("SSE3      : %s" % is_set(1, 2, 0))
+    print("SSSE3     : %s" % is_set(1, 2, 9))
+    print("SSE4.1    : %s" % is_set(1, 2, 19))
+    print("SSE4.2    : %s" % is_set(1, 2, 20))
+    print("SSE4a     : %s" % is_set(0x80000001, 2, 6))
+    print("AVX       : %s" % is_set(1, 2, 28))
+    print("AVX2      : %s" % is_set(7, 1, 5))
+    print("BMI1      : %s" % is_set(7, 1, 3))
+    print("BMI2      : %s" % is_set(7, 1, 8))
+
+
+    print("info from cpu_info package : {}\n".format(get_cpu_info()))
+
+
+
+
