ad9ed2be97e32d9179bc65edac1585d51e72bb53
Samuel Rince <s@rince.me>
2022-03-18 17:11:56 +0100

    add xml support

--- SERVER_AGENT/AGENT/agent/ram.py
+++ SERVER_AGENT/AGENT/agent/ram.py
@@ -1,6 +1,8 @@
 import json
 import subprocess
 
+import xml.etree.ElementTree as et
+
 from typing import List
 
 
@@ -11,19 +13,22 @@ CONVERT_KB_IN_GB = 9.536e-7
 def get_ram_info():
     try:
         return get_ram_info_with_lshw()
-    except (PermissionError, KeyError):
+    except json.JSONDecodeError:
+        print('failled parsing json file from lshw, trying with xml output')
+        return get_ram_info_with_lshw_xml()
+    except Exception:
         print('cannot access lshw without super-user using meminfo as fallback method')
         return get_ram_info_with_meminfo()
 
 
 def get_ram_info_with_lshw() -> List[dict]:
-    lshw_out = exec_lshw()
+    lshw_out = exec_lshw_json()
     memory_banks = parse_lshw_for_memory_components(lshw_out)
     memory_banks_info = parse_memory_children(memory_banks)
     return memory_banks_info
 
 
-def exec_lshw() -> dict:
+def exec_lshw_json() -> dict:
     proc = subprocess.Popen(['lshw', '-json'], stdout=subprocess.PIPE)
     out = proc.stdout.read()
     return json.loads(out)
@@ -53,6 +58,31 @@ def parse_memory_children(memory_children: List[dict]) -> List[dict]:
     return memory_banks_info
 
 
+def get_ram_info_with_lshw_xml() -> List[dict]:
+    xml = exec_lshw_xml()
+    memory_banks_info = parse_lshw_xml_for_memory_components(xml)
+    return memory_banks_info
+
+
+def exec_lshw_xml():
+    proc = subprocess.Popen(['lshw', '-xml'], stdout=subprocess.PIPE)
+    out = proc.stdout.read()
+    return et.fromstring(str(out))
+
+
+def parse_lshw_xml_for_memory_components(xml) -> List[dict]:
+    memory_banks = []
+    for memory in xml.findall(".//*[@id='memory']"):
+        for bank in memory.findall(".//*[@class='memory']"):
+            size = int(bank.find('size').text)
+            memory_banks.append({
+                'manufacturer': bank.find('vendor').text,
+                'model': bank.find('product').text,
+                'capacity': convert_bytes_in_gb(size)
+            })
+    return memory_banks
+
+
 def get_ram_info_with_meminfo() -> List[dict]:
     memory_size_kb = get_total_memory_in_kb()
     memory_size_gb = convert_kb_in_gb(memory_size_kb)
@@ -80,4 +110,4 @@ def convert_bytes_in_gb(value: int) -> float:
 
 
 if __name__ == '__main__':
-    print(get_ram_info())
+    get_ram_info()
