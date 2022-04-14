25426a929bc8ccabdc37d490753ca55ec18835d7
Benoit Petit <bpetit@nce.re>
2022-04-14 11:42:10 +0200

    validated link between hardware spec and impact api

--- SERVER_AGENT/agent/main.py
+++ SERVER_AGENT/agent/main.py
@@ -3,6 +3,7 @@
 from openapi_client import ApiClient, Configuration
 from openapi_client.api.component_api import ComponentApi
 from openapi_client.model.cpu import Cpu
+from openapi_client.model.ram import Ram
 
 from pprint import pprint
 
@@ -17,14 +18,39 @@ def main():
     pprint(ram)
     cpu = get_cpu()
     pprint(cpu)
-    call_api()
+    call_api(cpu, ram, disks)
 
 def get_disks():
     disk = Disk()
     disk.lookup()
     return disk
 
-def call_api():
+def extract_cpu(cpu_info):
+    request = {
+        "units": 1,
+        "core_units": cpu_info["cpu_info"]["count"],
+        "family": cpu_info["microarch"][0],
+        #"manufacture_date": "2017",
+        #"die_size_per_core": 2.0
+    }
+    return request
+
+def extract_ram(ram_info):
+    request = {
+        "units": len(ram_info),
+        "capacity": ram_info[0].size_gb,
+        "manufacturer": ram_info[0].manufacturer,
+        "model": ram_info[0].model
+    }
+    return request
+
+def extract_disks(disks_info):
+    request = {
+
+    }
+    return request
+
+def call_api(cpu, ram, disks):
     config = Configuration(
         host="http://localhost:5000",
     )
@@ -34,23 +60,32 @@ def call_api():
 
     component_api = ComponentApi(client)
 
-    kwargs={
-        "units": 1,
-        "core_units": 24,
-        "family": "Skylake",
-        "manufacture_date": "2017",
-        "die_size_per_core": 2.0
-    }
+    kwargs_cpu = extract_cpu(cpu)
+    print("############# request CPU ############")
+    pprint(kwargs_cpu)
 
-    cpu = Cpu(
-        **kwargs
-    )
+    cpu = Cpu(**kwargs_cpu)
+    #TODO : check CPU Name case sensistive or not
 
-    res = component_api.cpu_impact_bottom_up_v1_component_cpu_post(
-        cpu=cpu
-    )
+    res_cpu = component_api.cpu_impact_bottom_up_v1_component_cpu_post(cpu=cpu)
 
-    pprint(res)
+    print("############# answer CPU ############")
+    pprint(res_cpu)
+
+    kwargs_ram = extract_ram(ram)
+    print("############# request RAM ############")
+    pprint(kwargs_ram)
+
+    ram = Ram(**kwargs_ram)
+
+    res_ram = component_api.ram_impact_bottom_up_v1_component_ram_post(ram=ram)
+
+    print("############# answer RAM ############")
+    pprint(res_ram)
+
+    kwargs_disks = extract_disks(disks)
+    print("############# request Disks ############")
+    pprint(kwargs_cpu)
 
 if __name__ == '__main__':
     main()
