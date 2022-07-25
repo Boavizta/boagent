cc5cecf92d5e9e01ef9652c1566e788ed22a4e0d
Benoit Petit <bpetit@nce.re>
2022-07-25 18:32:15 +0200

    updated init__.py

--- boagent/__init__.py
+++ boagent/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '0.0.1'
+"""
+Boagent
+
+Monitoring agent/framework for evaluating the environmental impacts of a machine and its applications, including several to all steps of the life cycle of the machine and service, plus multiple criterias of impacts (not just CO2eq metrics / Global Warming Potential). Part of the efforts of https://boavizta.org/en and https://sdialliance.org/.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Benoit Petit <bpetit@hubblo.org>'
+__credits__ = 'Boavizta contributors'
