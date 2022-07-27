96dd30150ee57b4f37866b9d422afda84fe79ca1
Benoit Petit <bpetit@nce.re>
2022-07-27 08:40:46 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 from boagent import __version__
 
 py_version = sys.version_info[:2]
-if py_version < (3, 7):
-    raise Exception("api requires Python >= 3.7.")
+if py_version < (3, 9):
+    raise Exception("api requires Python >= 3.9.")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -16,7 +16,7 @@ setup(name='boagent',
       version=__version__,
       packages=find_packages(),
       include_package_data=True,
-      description="Local API and monitoring agent focussed on exposing environmental impacts metrics of the host. Based on the Boavizta API and Scaphandre.",
+      description="Monitoring agent/framework for evaluating the environmental impacts of a machine and its applications, including several to all steps of the life cycle of the machine and service, plus multiple criterias of impacts (not just CO2eq metrics / Global Warming Potential). Part of the efforts of https://boavizta.org/en and https://sdialliance.org/.",
       use_pipfile=True,
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -25,12 +25,9 @@ setup(name='boagent',
       setup_requires=['setuptools-pipfile'],
       keywords=['carbon', 'footprint', 'environment', 'climate', 'co2', 'gwp', 'adp', 'pe', 'energy', 'boagent', 'scaphandre', 'boavizta', 'api'],
       classifiers=[
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
           "Programming Language :: Python :: 3.9",
           "Intended Audience :: Developers",
           "Operating System :: OS Independent",
       ],
-      python_requires='>=3.7',
+      python_requires='>=3.9',
       entry_points=''' ''')
