613e1b61e0ae4f582567f4d552fff5ece0af7879
Benoit Petit <bpetit@hubblo.org>
2022-10-31 16:44:48 +0100

    fix: default config

--- boagent/api/config.py
+++ boagent/api/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "boagent"
     PROJECT_VERSION: str = "0.1.0"
     seconds_in_one_year: int = 31536000
-    default_lifetime: float = os.getenv("DEFAULT_LIFETIME", 0.5)
+    default_lifetime: float = os.getenv("DEFAULT_LIFETIME", 5.0)
     hardware_file_path: str = os.getenv("HARDWARE_FILE_PATH", "./hardware_data.json")
     power_file_path: str = os.getenv("POWER_FILE_PATH", "./power_data.json")
     hardware_cli: str = os.getenv("HARDWARE_CLI", "../hardware/hardware.py")
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     assets_path: str = os.getenv("BOAGENT_ASSETS_PATH", "../public/assets")
     carbon_aware_api_endpoint: str = os.getenv("CARBON_AWARE_API_ENDPOINT", "https://carbon-aware-api.azurewebsites.net")
     carbon_aware_api_token: str = os.getenv("CARBON_AWARE_API_TOKEN")
-    azure_location: str = os.getenv("AZURE_LOCATION", "northeurope")
+    azure_location: str = os.getenv("AZURE_LOCATION", "francecentral")
 
     class Config:
         env_file = ".env"
