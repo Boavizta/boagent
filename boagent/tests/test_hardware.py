580fd070d066eb3cdb0a7c2f68500d0704994935
repair <benjamin.dromard@gmail.com>
2024-03-11 15:21:29 +0100

    test: testing lshw cpus and disks properties

--- boagent/tests/test_hardware.py
+++ boagent/tests/test_hardware.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
-import hardware.cpu as cpu
-import hardware.lshw as lshw
 
-hw = lshw.LSHW()
+# import hardware.cpu as cpu
+import hardware.lshw as lshw
+# import hardware.hardware_cli as hwcli
+
+hw = lshw.Lshw()
 
 
 class LshwTest(TestCase):
@@ -21,19 +23,41 @@ class LshwTest(TestCase):
 
         assert type(memory_data) is list
 
+    def test_read_cpus_properties(self):
+        cpu_data = hw.cpus
 
-class CpuTest(TestCase):
+        for cpu in cpu_data:
+            assert "vendor" in cpu
+            assert "name" in cpu
+            assert "core_units" in cpu
+            assert "units" in cpu
+            assert cpu["vendor"] is not None
+            assert cpu["name"] is not None
+            assert cpu["core_units"] is not None
+            assert cpu["units"] is not None
+
+    def test_read_disks_properties(self):
+        disks_data = hw.disks
+
+        for disk in disks_data:
+            assert "type" in disk
+            assert "model" in disk
+            assert "manufacturer" in disk
+            assert "capacity" in disk
+            assert disk["type"] == "ssd" or disk["type"] == "hdd" or disk["type"] == "unknown"
+
+
+""" class CpuTest(TestCase):
     def test_read_get_cpus(self):
         cpu_data = cpu.get_cpus()
 
         assert type(cpu_data) is list
         assert "vendor" in cpu_data[0]
         assert "name" in cpu_data[0]
+ """
 
 
-class DiskTest(TestCase):
-    pass
+class HardwarecliTest(TestCase):
 
-
-class RamTest(TestCase):
-    pass
+    def test_read_hardware_cli_cpus(self):
+        pass
