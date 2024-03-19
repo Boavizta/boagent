e02e921c39941615c27bf2987078e75698bc0ade
repair <benjamin.dromard@gmail.com>
2024-03-19 12:44:10 +0100

    test: format_usage_request with BoaviztAPI keys

--- boagent/api/test_api_unit.py
+++ boagent/api/test_api_unit.py
@@ -5,7 +5,8 @@ from api import (
     build_hardware_data,
     read_hardware_data,
     get_hardware_data,
-    query_machine_impact_data,
+    # query_machine_impact_data,
+    format_usage_request,
 )
 import os
 
@@ -42,10 +43,32 @@ class ApiUnitTest(TestCase):
         data = get_hardware_data(fetch_hardware=True)
         assert type(data) is dict
 
-    def test_read_query_machine_impact_data(self):
-        server_impact = query_machine_impact_data()
-        print(server_impact)
-        pass
+    # def test_read_query_machine_impact_data(self):
+    #    server_impact = query_machine_impact_data()
+    #    print(server_impact)
+    #    pass
+
+    def test_read_format_usage_request_with_host_avg_consumption_and_use_time_ratio(
+        self,
+    ):
+
+        start_time = 1710837858
+        end_time = 1710841458
+        location = "FRA"
+        host_avg_consumption = 120
+        use_time_ratio = 1
+
+        formatted_request = format_usage_request(
+            start_time=start_time,
+            end_time=end_time,
+            location=location,
+            host_avg_consumption=host_avg_consumption,
+            use_time_ratio=use_time_ratio,
+        )
+        assert type(formatted_request) is dict
+        assert "avg_power" in formatted_request
+        assert "use_time_ratio" in formatted_request
+        assert "usage_location" in formatted_request
 
     def tearDown(self) -> None:
         os.remove("./hardware_data.json")
