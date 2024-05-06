440ff985ccd44d66d3c829639df40aa0742ff4d9
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-05-06 17:14:31 +0200

    docs: change version in init

--- boagent/__init__.py
+++ boagent/__init__.py
@@ -4,6 +4,6 @@ Boagent
 Monitoring agent/framework for evaluating the environmental impacts of a machine and its applications, including several to all steps of the life cycle of the machine and service, plus multiple criterias of impacts (not just CO2eq metrics / Global Warming Potential). Part of the efforts of https://boavizta.org/en and https://sdialliance.org/.
 """
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 __author__ = "Benoit Petit <bpetit@hubblo.org>"
 __credits__ = "Boavizta contributors"
