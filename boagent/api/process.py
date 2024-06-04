5a8f8ea9226afbe03630ae57652b6c1998079b51
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-06-04 15:58:02 +0200

    refactor: calculate abstract component embedded impact shares instead of specific component

--- boagent/api/process.py
+++ boagent/api/process.py
@@ -9,8 +9,13 @@ class Process:
         self.process_info = self.get_process_info()
         self.total_ram_in_bytes = self.get_total_ram_in_bytes()
         self.ram_shares = self.get_ram_shares()
-        self.ram_embedded_impact_shares = self.get_ram_embedded_impact_shares()
+        self.ram_embedded_impact_shares = self.get_component_embedded_impact_shares(
+            "ram", self.ram_shares
+        )
         self.cpu_load_shares = self.get_cpu_load_shares()
+        self.cpu_embedded_impact_shares = self.get_component_embedded_impact_shares(
+            "cpu", self.cpu_load_shares
+        )
 
     def process_metrics(self):
 
@@ -47,30 +52,6 @@ class Process:
 
         return process_ram_shares
 
-    def get_ram_embedded_impact_shares(self):
-
-        ram_impacts_data = self.processed_metrics["raw_data"]["boaviztapi_data"][
-            "verbose"
-        ]["RAM-1"]["impacts"]
-        ram_embedded_impact_shares = list()
-        for impact in ram_impacts_data:
-            impact_embedded_value = ram_impacts_data[impact]["embedded"]["value"]
-            for process_ram_share in self.ram_shares:
-                if process_ram_share == 0.0:
-                    ram_embedded_impact = {
-                        f"{impact}_embedded_share": process_ram_share
-                    }
-                    ram_embedded_impact_shares.append(ram_embedded_impact)
-                else:
-                    ram_embedded_impact_share = round(
-                        impact_embedded_value * process_ram_share, 2
-                    )
-                    ram_embedded_impact = {
-                        f"{impact}_embedded_share": ram_embedded_impact_share
-                    }
-                    ram_embedded_impact_shares.append(ram_embedded_impact)
-        return ram_embedded_impact_shares
-
     def get_cpu_load_shares(self):
 
         process_cpu_load_shares = list()
@@ -79,3 +60,28 @@ class Process:
             process_cpu_load_shares.append(process_cpu_load_share)
 
         return process_cpu_load_shares
+
+    def get_component_embedded_impact_shares(self, queried_component, component_shares):
+
+        component = f"{queried_component.upper()}-1"
+        component_impacts_data = self.processed_metrics["raw_data"]["boaviztapi_data"][
+            "verbose"
+        ][component]["impacts"]
+        component_embedded_impact_shares = list()
+        for impact in component_impacts_data:
+            impact_embedded_value = component_impacts_data[impact]["embedded"]["value"]
+            for process_component_share in component_shares:
+                if process_component_share == 0.0:
+                    component_embedded_impact = {
+                        f"{impact}_embedded_share": process_component_share
+                    }
+                    component_embedded_impact_shares.append(component_embedded_impact)
+                else:
+                    component_embedded_impact_share = round(
+                        impact_embedded_value * process_component_share, 2
+                    )
+                    component_embedded_impact = {
+                        f"{impact}_embedded_share": component_embedded_impact_share
+                    }
+                    component_embedded_impact_shares.append(component_embedded_impact)
+        return component_embedded_impact_shares
