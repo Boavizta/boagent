a0359148097ce3f12004983e59f40a5a753091c1
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-06-04 17:44:35 +0200

    feat: WIP calculate average values for each impact for each component

--- boagent/api/process.py
+++ boagent/api/process.py
@@ -10,11 +10,17 @@ class Process:
         self.total_ram_in_bytes = self.get_total_ram_in_bytes()
         self.ram_shares = self.get_ram_shares()
         self.ram_embedded_impact_shares = self.get_component_embedded_impact_shares(
-            "ram", self.ram_shares
+            "RAM", self.ram_shares
         )
         self.cpu_load_shares = self.get_cpu_load_shares()
         self.cpu_embedded_impact_shares = self.get_component_embedded_impact_shares(
-            "cpu", self.cpu_load_shares
+            "CPU", self.cpu_load_shares
+        )
+        self.cpu_average_embedded_impacts = self.get_component_average_embedded_impact(
+            "cpu"
+        )
+        self.ram_average_embedded_impacts = self.get_component_average_embedded_impact(
+            "ram"
         )
 
     def process_metrics(self):
@@ -46,7 +52,8 @@ class Process:
         process_ram_shares = list()
         for timestamp in self.process_info:
             process_ram_share = (
-                int(timestamp["resources_usage"]["memory_usage"]) / self.total_ram_bytes
+                int(timestamp["resources_usage"]["memory_usage"])
+                / self.total_ram_in_bytes
             )
             process_ram_shares.append(process_ram_share)
 
@@ -56,14 +63,14 @@ class Process:
 
         process_cpu_load_shares = list()
         for timestamp in self.process_info:
-            process_cpu_load_share = timestamp["resources_usage"]["cpu_usage"]
+            process_cpu_load_share = float(timestamp["resources_usage"]["cpu_usage"])
             process_cpu_load_shares.append(process_cpu_load_share)
 
         return process_cpu_load_shares
 
     def get_component_embedded_impact_shares(self, queried_component, component_shares):
 
-        component = f"{queried_component.upper()}-1"
+        component = f"{queried_component}-1"
         component_impacts_data = self.processed_metrics["raw_data"]["boaviztapi_data"][
             "verbose"
         ][component]["impacts"]
@@ -73,15 +80,40 @@ class Process:
             for process_component_share in component_shares:
                 if process_component_share == 0.0:
                     component_embedded_impact = {
-                        f"{impact}_embedded_share": process_component_share
+                        f"{impact}_embedded_share": float(process_component_share)
                     }
                     component_embedded_impact_shares.append(component_embedded_impact)
                 else:
                     component_embedded_impact_share = round(
-                        impact_embedded_value * process_component_share, 2
+                        float(impact_embedded_value) * float(process_component_share), 2
                     )
                     component_embedded_impact = {
-                        f"{impact}_embedded_share": component_embedded_impact_share
+                        f"{impact}_embedded_share": float(
+                            component_embedded_impact_share
+                        )
                     }
                     component_embedded_impact_shares.append(component_embedded_impact)
         return component_embedded_impact_shares
+
+    def get_component_average_embedded_impact(self, queried_component):
+
+        embedded_impacts_sums = {
+            impact_criteria: sum(
+                impact_dictionary[impact_criteria]
+                for impact_dictionary in self.ram_embedded_impact_shares
+                if impact_criteria in impact_dictionary
+            )
+            for impact_criteria in set(
+                impact_criteria
+                for impact_dictionary in self.ram_embedded_impact_shares
+                for impact_criteria in impact_dictionary
+            )
+        }
+
+        average_values = list()
+        for impact_criteria in embedded_impacts_sums:
+            average_result = embedded_impacts_sums[impact_criteria] / (
+                len(self.ram_embedded_impact_shares) / 3
+            )
+            average_value = {f"{impact_criteria}_average": average_result}
+            average_values.append(average_value)
