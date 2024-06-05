150908f7d2bed4a12f909108416493ad558c61f9
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-06-05 16:11:34 +0200

    feat: get max and min in addition of average values, refactored a few functions with list comprehensions

--- boagent/api/process.py
+++ boagent/api/process.py
@@ -1,4 +1,4 @@
-import json
+from json import load
 from collections import defaultdict
 
 
@@ -8,26 +8,14 @@ class Process:
         self.pid = pid
         self.processed_metrics = self.process_metrics()
         self.process_info = self.get_process_info()
-        self.total_ram_in_bytes = self.get_total_ram_in_bytes()
+        self.process_name = self.get_process_name()
         self.ram_shares = self.get_ram_shares()
-        self.ram_embedded_impact_shares = self.get_component_embedded_impact_shares(
-            "RAM", self.ram_shares
-        )
         self.cpu_load_shares = self.get_cpu_load_shares()
-        self.cpu_embedded_impact_shares = self.get_component_embedded_impact_shares(
-            "CPU", self.cpu_load_shares
-        )
-        self.cpu_average_embedded_impacts = self.get_component_average_embedded_impact(
-            "cpu"
-        )
-        self.ram_average_embedded_impacts = self.get_component_average_embedded_impact(
-            "ram"
-        )
 
     def process_metrics(self):
 
         with open(self.metrics_data, "r") as metrics_data_file:
-            metrics_data = json.load(metrics_data_file)
+            metrics_data = load(metrics_data_file)
 
         return metrics_data
 
@@ -40,6 +28,10 @@ class Process:
                     process_info.append(process)
         return process_info
 
+    def get_process_name(self):
+        process_name = self.process_info[0]["exe"].split("/")[-1]
+        return process_name
+
     def get_total_ram_in_bytes(self):
 
         ram_data = self.processed_metrics["raw_data"]["hardware_data"]["rams"]
@@ -50,21 +42,26 @@ class Process:
         return total_ram_in_bytes
 
     def get_ram_shares(self):
-        process_ram_shares = list()
-        for timestamp in self.process_info:
-            process_ram_share = (
-                int(timestamp["resources_usage"]["memory_usage"])
-                / self.total_ram_in_bytes
-            ) * 100
-            process_ram_shares.append(process_ram_share)
+
+        process_ram_shares = [
+            (
+                (
+                    int(timestamp["resources_usage"]["memory_usage"])
+                    / self.get_total_ram_in_bytes()
+                )
+                * 100
+            )
+            for timestamp in self.process_info
+        ]
+
         return process_ram_shares
 
     def get_cpu_load_shares(self):
 
-        process_cpu_load_shares = list()
-        for timestamp in self.process_info:
-            process_cpu_load_share = float(timestamp["resources_usage"]["cpu_usage"])
-            process_cpu_load_shares.append(process_cpu_load_share)
+        process_cpu_load_shares = [
+            float(timestamp["resources_usage"]["cpu_usage"])
+            for timestamp in self.process_info
+        ]
         return process_cpu_load_shares
 
     def get_component_embedded_impact_shares(self, queried_component, component_shares):
@@ -94,15 +91,19 @@ class Process:
                     component_embedded_impact_shares.append(component_embedded_impact)
         return component_embedded_impact_shares
 
-    def get_component_average_embedded_impact(self, queried_component):
+    def get_component_embedded_impact_values(self, queried_component):
+        if queried_component == "cpu":
+            component_impact_shares = self.get_component_embedded_impact_shares(
+                "CPU", self.cpu_load_shares
+            )
+        if queried_component == "ram":
+            component_impact_shares = self.get_component_embedded_impact_shares(
+                "RAM", self.ram_shares
+            )
+
         gwp_list = defaultdict(list)
         adp_list = defaultdict(list)
         pe_list = defaultdict(list)
-
-        if queried_component == "cpu":
-            component_impact_shares = self.cpu_embedded_impact_shares
-        if queried_component == "ram":
-            component_impact_shares = self.ram_embedded_impact_shares
 
         for impact_key, impact_value in component_impact_shares:
             if impact_key == "gwp_embedded_share":
@@ -122,9 +123,23 @@ class Process:
             pe_list["pe_embedded_share"]
         )
 
-        component_average_impacts = {
+        gwp_max = max(gwp_list["gwp_embedded_share"])
+        adp_max = max(adp_list["adp_embedded_share"])
+        pe_max = max(pe_list["pe_embedded_share"])
+
+        gwp_min = min(gwp_list["gwp_embedded_share"])
+        adp_min = min(adp_list["adp_embedded_share"])
+        pe_min = min(pe_list["pe_embedded_share"])
+
+        component_embedded_impact_values = {
             f"gwp_{queried_component}_average_impact": gwp_average,
             f"adp_{queried_component}_average_impact": adp_average,
             f"pe_{queried_component}_average_impact": pe_average,
+            f"gwp_{queried_component}_max_impact": gwp_max,
+            f"adp_{queried_component}_max_impact": adp_max,
+            f"pe_{queried_component}_max_impact": pe_max,
+            f"gwp_{queried_component}_min_impact": gwp_min,
+            f"adp_{queried_component}_min_impact": adp_min,
+            f"pe_{queried_component}_min_impact": pe_min,
         }
-        return component_average_impacts
+        return component_embedded_impact_values
