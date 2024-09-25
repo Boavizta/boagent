725e9cbecb60c3108825a02dc18258cbe2ad1070
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-09-25 12:19:14 +0200

    refactor: error handling with click, lshw instance in main

--- hardware_cli.py
+++ hardware_cli.py
@@ -6,41 +6,31 @@ import sys
 
 from boagent.hardware.lshw import Lshw
 
-lshw = Lshw()
-
-lshw_cpus = lshw.cpus
-lshw_ram = lshw.memories
-lshw_disks = lshw.disks
-
 
 @click.command()
 @click.option("--output-file", help="File to output the hardwate data to")
 def main(output_file):
-    hardware_data = {}
-    hardware_data["disks"] = get_disks()
-    hardware_data["cpus"] = get_cpus()
-    hardware_data["rams"] = get_ram()
-    if output_file is not None:
-        with open(output_file, "w") as fd:
-            json.dump(hardware_data, fd, indent=4)
+    try:
+        lshw = Lshw()
+
+        lshw_cpus = lshw.cpus
+        lshw_ram = lshw.memories
+        lshw_disks = lshw.disks
+    except KeyError:
+        error_message = "Hardware_cli was not executed with privileges, try `sudo ./hardware_cli.py`."
+        exception = click.ClickException(error_message)
+        exception.show()
     else:
-        json.dump(hardware_data, sys.stdout, indent=4)
-    return 0
-
-
-def get_disks():
-    disks = lshw_disks
-    return disks
-
-
-def get_cpus():
-    cpus = lshw_cpus
-    return cpus
-
-
-def get_ram():
-    rams = lshw_ram
-    return rams
+        hardware_data = {}
+        hardware_data["disks"] = lshw_disks
+        hardware_data["cpus"] = lshw_cpus
+        hardware_data["rams"] = lshw_ram
+        if output_file is not None:
+            with open(output_file, "w") as fd:
+                json.dump(hardware_data, fd, indent=4)
+        else:
+            json.dump(hardware_data, sys.stdout, indent=4)
+        return 0
 
 
 if __name__ == "__main__":
