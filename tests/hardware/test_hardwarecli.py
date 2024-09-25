9d74841ece0cde6490ae03a3a73b5ce53139d544
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-09-25 12:21:47 +0200

    fix: mock lshw, lshw attributes to avoid side effects

--- tests/hardware/test_hardwarecli.py
+++ tests/hardware/test_hardwarecli.py
@@ -1,25 +1,54 @@
+from json import load
 from unittest import TestCase
+from os import path
 from os.path import exists
-from hardware_cli import main, get_cpus, get_ram, get_disks
+from unittest.mock import Mock, patch
+from hardware_cli import main
 from click.testing import CliRunner
+
+current_dir = path.dirname(__file__)
+mock_lshw_data = path.join(f"{current_dir}", "../mocks/sudo_lshw_data")
+with open(f"{mock_lshw_data}.json") as lshw_json:
+    data = load(lshw_json)
+
+
+class MockLshw:
+    def __init__(self):
+        self.cpus = {
+            "cpus": [
+                {
+                    "units": 1,
+                    "name": "AMD Ryzen 5 5600H with Radeon Graphics",
+                    "manufacturer": "Advanced Micro Devices [AMD]",
+                    "core_units": 6,
+                }
+            ]
+        }
+        self.memories = {
+            "rams": [
+                {"units": 1, "manufacturer": "Samsung", "capacity": 8},
+                {"units": 1, "manufacturer": "Kingston", "capacity": 16},
+            ]
+        }
+        self.disks = {
+            "disks": [
+                {
+                    "units": 1,
+                    "logicalname": "/dev/nvme0n1",
+                    "manufacturer": "samsung",
+                    "type": "ssd",
+                    "capacity": 476,
+                }
+            ],
+        }
+
+
+mocked_lshw = Mock()
+mocked_lshw.return_value = MockLshw()
 
 
 class HardwarecliTest(TestCase):
-    def test_read_hardware_cli_cpus(self):
-
-        cpus = get_cpus()
-        assert type(cpus) is list
-
-    def test_read_hardware_cli_ram(self):
-
-        ram = get_ram()
-        assert type(ram) is list
-
-    def test_read_hardware_cli_disks(self):
-
-        disks = get_disks()
-        assert type(disks) is list
-
+    @patch("hardware_cli.Lshw", mocked_lshw)
     def test_write_hardware_json_file_from_hardware_cli_with_output_file_flag_on(self):
 
         runner = CliRunner()
@@ -31,6 +60,7 @@ class HardwarecliTest(TestCase):
 
         assert result.exit_code == 0
 
+    @patch("hardware_cli.Lshw", mocked_lshw)
     def test_read_stdout_from_hardware_cli(self):
 
         runner = CliRunner()
@@ -41,3 +71,12 @@ class HardwarecliTest(TestCase):
         assert result.output.count("disk") >= 1
         assert result.output.count("ram") >= 1
         assert result.output.count("cpu") >= 1
+
+    def test_hardware_cli_returns_error_is_not_executed_with_sudo(self):
+        runner = CliRunner()
+        result = runner.invoke(main)
+        assert (
+            result.output.__contains__(
+                "Hardware_cli was not executed with privileges, try `sudo ./hardware_cli.py`"
+            )
+        ) is True
--- tests/hardware/test_lshw.py
+++ tests/hardware/test_lshw.py
@@ -1,77 +1,88 @@
 from unittest import TestCase
 from boagent.hardware.lshw import Lshw
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 from json import load
 from os import path
 
 current_dir = path.dirname(__file__)
 mock_lshw_data = path.join(f"{current_dir}", "../mocks/sudo_lshw_data")
+with open(f"{mock_lshw_data}.json") as lshw_json:
+    data = load(lshw_json)
 
-hw = Lshw()
-
-lshw_cpus_data = hw.cpus
-lshw_disks_data = hw.disks
-lshw_ram_data = hw.memories
+mocked_is_tool = Mock()
+mocked_is_tool.return_value = True
+mocked_serialized_lshw_output = Mock()
+mocked_serialized_lshw_output.return_value = data
 
 
 class LshwTest(TestCase):
+    @patch("boagent.hardware.lshw.is_tool", mocked_is_tool)
+    @patch(
+        "boagent.hardware.lshw.serialized_lshw_output", mocked_serialized_lshw_output
+    )
+    def setUp(self):
+        self.lshw = Lshw()
+        self.cpu_data = self.lshw.cpus
+        self.storage_data = self.lshw.disks
+        self.ram_data = self.lshw.memories
+
     def test_read_get_hw_linux_cpu(self):
-        cpu_data = hw.get_hw_linux("cpu")
+        cpu_data = self.lshw.get_hw_linux("cpu")
 
         assert type(cpu_data) is list
 
     def test_read_get_hw_linux_storage(self):
-        storage_data = hw.get_hw_linux("storage")
+        storage_data = self.lshw.get_hw_linux("storage")
 
         assert type(storage_data) is list
 
     def test_read_get_hw_linux_memory(self):
-        memory_data = hw.get_hw_linux("memory")
+        memory_data = self.lshw.get_hw_linux("memory")
 
         assert type(memory_data) is list
 
     def test_read_cpus_vendor(self):
 
-        for cpu in lshw_cpus_data:
+        for cpu in self.cpu_data:
             assert "manufacturer" in cpu
             assert type(cpu["manufacturer"]) is str
 
     def test_read_cpus_name(self):
 
-        for cpu in lshw_cpus_data:
+        for cpu in self.cpu_data:
             assert "name" in cpu
             assert type(cpu["name"]) is str
 
     def test_read_cpus_core_units(self):
 
-        for cpu in lshw_cpus_data:
+        for cpu in self.cpu_data:
             assert "core_units" in cpu
             assert type(cpu["core_units"]) is int
 
     def test_read_cpus_units(self):
 
-        for cpu in lshw_cpus_data:
+        for cpu in self.cpu_data:
             assert "units" in cpu
             assert type(cpu["units"]) is int
 
     def test_read_check_disk_vendor_with_correct_model(self):
 
         model = "LENOVO 123456154"
-        result = hw.check_disk_vendor(model)
+        result = self.lshw.check_disk_vendor(model)
 
         assert result == "LENOVO"
 
     def test_read_check_disk_vendor_with_incorrect_model(self):
 
         model = "12345121 LENOVO"
-        result = hw.check_disk_vendor(model)
+        result = self.lshw.check_disk_vendor(model)
 
         assert result == "LENOVO"
 
     def test_read_check_disk_vendor_with_one_correct_string_in_model(self):
 
         model = "LENOVO"
-        result = hw.check_disk_vendor(model)
+        result = self.lshw.check_disk_vendor(model)
 
         assert result == "LENOVO"
 
@@ -79,18 +90,18 @@ class LshwTest(TestCase):
 
         model = "12345211"
         with self.assertRaises(Exception):
-            hw.check_disk_vendor(model)
+            self.lshw.check_disk_vendor(model)
 
     def test_read_check_disk_vendor_with_multiple_strings_in_model(self):
 
         model = "LENOVO 123456 MODEL"
-        result = hw.check_disk_vendor(model)
+        result = self.lshw.check_disk_vendor(model)
 
         assert result == "LENOVO"
 
     def test_read_disks_type(self):
 
-        for disk in lshw_disks_data:
+        for disk in self.storage_data:
             assert "type" in disk
             assert type(disk["type"]) is str
             assert (
@@ -102,7 +113,7 @@ class LshwTest(TestCase):
 
     def test_read_disk_dev_name(self):
 
-        for disk in lshw_disks_data:
+        for disk in self.storage_data:
             assert "logicalname" in disk
             assert type(disk["logicalname"]) is str
 
@@ -112,7 +123,7 @@ class LshwTest(TestCase):
         dev_logicalname = "/dev/ssdonsata"
         mocked_get_rotational.return_value = 0
 
-        disk_type = hw.get_disk_type(dev_logicalname)
+        disk_type = self.lshw.get_disk_type(dev_logicalname)
         assert disk_type == "ssd"
 
     @patch("boagent.hardware.lshw.Lshw.get_rotational_int")
@@ -121,30 +132,29 @@ class LshwTest(TestCase):
         dev_logicalname = "/dev/sdaex"
         mocked_get_rotational.return_value = 1
 
-        disk_type = hw.get_disk_type(dev_logicalname)
+        disk_type = self.lshw.get_disk_type(dev_logicalname)
         assert disk_type == "hdd"
 
     def test_int_for_get_rotational_int_when_file_not_found(self):
 
         dev_erroneous_name = "/dev/thisnameleadstonorotational"
-        rotational_int = hw.get_rotational_int(dev_erroneous_name)
+        rotational_int = self.lshw.get_rotational_int(dev_erroneous_name)
 
         self.assertEqual(rotational_int, 2)
 
     def test_read_disk_type_when_dev_path_not_found(self):
 
         dev_erroneous_name = "/dev/thisnamedoesntexist"
-        disk_type = hw.get_disk_type(dev_erroneous_name)
+        disk_type = self.lshw.get_disk_type(dev_erroneous_name)
         assert disk_type == "unknown"
 
     @patch("boagent.hardware.lshw.is_tool")
     def test_check_lshw_is_installed_to_parse_hardware_data_and_raises_error_if_not(
         self, mocked_is_tool
     ):
-        another_lshw = Lshw()
         mocked_is_tool.return_value = False
         with self.assertRaises(Exception) as context:
-            another_lshw.__init__()
+            self.lshw.__init__()
         self.assertTrue("lshw does not seem to be installed" in str(context.exception))
 
     @patch("boagent.hardware.lshw.is_tool")
@@ -157,42 +167,42 @@ class LshwTest(TestCase):
             Exception
         ) as nvme_cli_exception:
             data = load(file)
-            hw.find_storage(data)
+            self.lshw.find_storage(data)
 
         caught_exception = nvme_cli_exception.exception
         assert str(caught_exception) == "nvme-cli >= 1.0 does not seem to be installed"
 
     def test_read_disks_manufacturer(self):
 
-        for disk in lshw_disks_data:
+        for disk in self.storage_data:
             assert "manufacturer" in disk
             assert type(disk["manufacturer"]) is str
 
     def test_read_disks_capacity(self):
 
-        for disk in lshw_disks_data:
+        for disk in self.storage_data:
             assert "capacity" in disk
             assert type(disk["capacity"]) is int
 
     def test_read_disks_units(self):
 
-        for disk in lshw_disks_data:
+        for disk in self.storage_data:
             assert "units" in disk
             assert type(disk["units"]) is int
 
     def test_read_ram_manufacturer(self):
 
-        for ram in lshw_ram_data:
+        for ram in self.ram_data:
             assert "manufacturer" in ram
             assert type(ram["manufacturer"]) is str
 
     def test_read_ram_capacity(self):
 
-        for ram in lshw_ram_data[1:]:
+        for ram in self.ram_data[1:]:
             assert "capacity" in ram
             assert type(ram["capacity"]) is int
 
     def test_read_ram_units(self):
 
-        assert "units" in lshw_ram_data[0]
-        assert type(lshw_ram_data[0]["units"]) is int
+        assert "units" in self.ram_data[0]
+        assert type(self.ram_data[0]["units"]) is int
