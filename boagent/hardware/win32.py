276cbd186f13ab91bc463c688329d0346beb465f
B. Dromard <bdromard@tutanota.com>
2026-01-15 16:02:04 +0100

    refactor(windows): add wmi class enum

--- boagent/hardware/win32.py
+++ boagent/hardware/win32.py
@@ -1,4 +1,15 @@
 import win32com.client
+from enum import Enum
+
+Win32_WMI_Class = Enum(
+    "Win32_WMI_Class",
+    [
+        ("PROCESSOR", "Win32_Processor"),
+        ("MEMORY", "Win32_PhysicalMemory"),
+        ("DRIVE", "Win32_DiskDrive"),
+        ("LOGICAL_DRIVE", "Win32_LogicalDrive"),
+    ],
+)
 
 
 def get_property(com_object, field: str) -> str | int:
@@ -6,7 +17,7 @@ def get_property(com_object, field: str) -> str | int:
     return property
 
 
-def get_win32_instances(class_name: str):
+def get_win32_instances(class_name: Win32_WMI_Class):
     instances = win32com.client.GetObject("winmgmts:").InstancesOf(class_name)
     return instances
 
@@ -23,7 +34,7 @@ class Hardware:
 
     def find_cpus(self) -> dict:
         cpus = {"cpus": []}
-        processors = get_win32_instances("Win32_Processor")
+        processors = get_win32_instances(Win32_WMI_Class.PROCESSOR)
         total_units = len(processors)
         for processor in processors:
             cpu_name = get_property(processor, "Name")
@@ -41,7 +52,7 @@ class Hardware:
 
     def find_memories(self) -> dict:
         memories = {"rams": []}
-        rams = get_win32_instances("Win32_PhysicalMemory")
+        rams = get_win32_instances(Win32_WMI_Class.MEMORY)
         total_units = len(rams)
         for ram in rams:
             ram_manufacturer = get_property(ram, "Manufacturer")
