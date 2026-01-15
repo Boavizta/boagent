6710d06d131f9063bf58555fbd974ea9a067c316
B. Dromard <bdromard@tutanota.com>
2026-01-15 11:45:35 +0100

    feat(windows): find ram units

--- boagent/hardware/win32.py
+++ boagent/hardware/win32.py
@@ -1,7 +1,7 @@
 import win32com.client
 
 
-def get_property(com_object, field: str) -> str:
+def get_property(com_object, field: str) -> str | int:
     property = com_object.Properties_(field).Value
     return property
 
@@ -11,18 +11,26 @@ def get_win32_instances(class_name: str):
     return instances
 
 
+def convert_to_gigabytes(capacity: int):
+    capacity_to_gigabytes = capacity // 2**20 // 1024
+    return capacity_to_gigabytes
+
+
 class Hardware:
     def __init__(self) -> None:
         self.cpus = []
+        self.memories = []
 
     def find_cpus(self) -> dict:
         cpus = {"cpus": []}
         processors = get_win32_instances("Win32_Processor")
+        total_units = len(processors)
         for processor in processors:
             cpu_name = get_property(processor, "Name")
             cpu_manufacturer = get_property(processor, "Manufacturer")
             cpu_core_units = get_property(processor, "NumberOfCores")
             cpu = {
+                "units": total_units,
                 "name": cpu_name,
                 "manufacturer": cpu_manufacturer,
                 "core_units": cpu_core_units,
@@ -30,3 +38,20 @@ class Hardware:
             cpus["cpus"].append(cpu)
 
         return cpus
+
+    def find_memories(self) -> dict:
+        memories = {"rams": []}
+        rams = get_win32_instances("Win32_PhysicalMemory")
+        total_units = len(rams)
+        for ram in rams:
+            ram_manufacturer = get_property(ram, "Manufacturer")
+            ram_capacity = get_property(ram, "Capacity")
+            capacity_converted_to_gigabytes = convert_to_gigabytes(ram_capacity)
+            ram = {
+                "units": total_units,
+                "manufacturer": ram_manufacturer,
+                "capacity": capacity_converted_to_gigabytes,
+            }
+            memories["rams"].append(ram)
+
+        return memories
--- tests/hardware/test_win32.py
+++ tests/hardware/test_win32.py
@@ -12,6 +12,8 @@ from boagent.hardware.win32 import Hardware
 mock_win32_processor_name = "AMD Ryzen 5 5600H with Radeon Graphics"
 mock_win32_processor_manufacturer = "Advanced Micro Devices [AMD]"
 mock_win32_processor_core_units = 6
+mock_win32_memory_manufacturer = "Samsung"
+mock_win32_memory_capacity = 8589934592
 
 
 class HardwareTest(TestCase):
@@ -26,7 +28,7 @@ class HardwareTest(TestCase):
         mocked_get_property.return_value = mock_win32_processor_name
         cpus = self.hardware.find_cpus()
         cpu_name = cpus["cpus"][0]["name"]
-        assert cpu_name == "AMD Ryzen 5 5600H with Radeon Graphics"
+        assert cpu_name == mock_win32_processor_name
 
     @patch("boagent.hardware.win32.get_win32_instances")
     @patch("boagent.hardware.win32.get_property")
@@ -35,7 +37,7 @@ class HardwareTest(TestCase):
         mocked_get_property.return_value = mock_win32_processor_manufacturer
         cpus = self.hardware.find_cpus()
         cpu_manufacturer = cpus["cpus"][0]["manufacturer"]
-        assert cpu_manufacturer == "Advanced Micro Devices [AMD]"
+        assert cpu_manufacturer == mock_win32_processor_manufacturer
 
     @patch("boagent.hardware.win32.get_win32_instances")
     @patch("boagent.hardware.win32.get_property")
@@ -44,4 +46,48 @@ class HardwareTest(TestCase):
         mocked_get_property.return_value = mock_win32_processor_core_units
         cpus = self.hardware.find_cpus()
         cpu_core_units = cpus["cpus"][0]["core_units"]
-        assert cpu_core_units == 6
+        assert cpu_core_units == mock_win32_processor_core_units
+
+    @patch("boagent.hardware.win32.get_win32_instances")
+    @patch("boagent.hardware.win32.get_property")
+    def test_find_cpu_units(self, mocked_get_property, mocked_win32_instances):
+        mocked_win32_instances.return_value = [1]
+        mocked_get_property.return_value = mock_win32_processor_core_units
+        cpus = self.hardware.find_cpus()
+        cpu_units = cpus["cpus"][0]["units"]
+        expected_units = 1
+        assert cpu_units == expected_units
+
+    @patch("boagent.hardware.win32.get_win32_instances")
+    @patch("boagent.hardware.win32.get_property")
+    def test_find_memory_manufacturer(
+        self, mocked_get_property, mocked_win32_instances
+    ):
+        mocked_win32_instances.return_value = [1]
+        mocked_get_property.return_value = mock_win32_memory_manufacturer
+        memories = self.hardware.find_memories()
+        memory_manufacturer = memories["rams"][0]["manufacturer"]
+        assert memory_manufacturer == mock_win32_memory_manufacturer
+
+    @patch("boagent.hardware.win32.get_win32_instances")
+    @patch("boagent.hardware.win32.get_property")
+    def test_find_memory_capacity(self, mocked_get_property, mocked_win32_instances):
+        mocked_win32_instances.return_value = [1]
+        mocked_get_property.return_value = mock_win32_memory_capacity
+        memories = self.hardware.find_memories()
+        memory_capacity = memories["rams"][0]["capacity"]
+        expected_capacity_in_gigabytes = 8
+        assert memory_capacity == expected_capacity_in_gigabytes
+
+    @patch("boagent.hardware.win32.get_win32_instances")
+    @patch("boagent.hardware.win32.get_property")
+    @patch("boagent.hardware.win32.convert_to_gigabytes")
+    def test_find_memory_units(
+        self, mocked_conversion, mocked_get_property, mocked_win32_instances
+    ):
+        mocked_win32_instances.return_value = [1, 2]
+        mocked_get_property.return_value = 1
+        mocked_conversion.return_value = 8
+        memories = self.hardware.find_memories()
+        memory_units = memories["rams"][0]["units"]
+        assert memory_units == 2
