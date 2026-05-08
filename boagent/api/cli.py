3a86eb360730d94812e17b7b9639c3193e61fb6e
Benoit Petit <bpetit@hubblo.org>
2026-05-08 10:24:59 +0200

    feat: added signal handling and proper loop to cli endpoint

--- boagent/api/cli.py
+++ boagent/api/cli.py
@@ -1,9 +1,17 @@
 import click
 import requests
 import json
+import time
 from os import uname
+from sys import exit
+import signal
 from boagent.api.api import get_metrics, DEFAULT_LIFETIME
 from boagent.api.utils import iso8601_or_timestamp_as_timestamp, format_prometheus_metric, format_prometheus_output
+
+
+def handler(signum, frame):
+    print("Signal handler called with signal {}".format(signum))
+    exit(signum)
 
 @click.command("prometheus-push")
 @click.option('--push-url', default='http://localhost:9091',  help='Push Gateway URL.')
@@ -17,32 +25,56 @@ from boagent.api.utils import iso8601_or_timestamp_as_timestamp, format_promethe
 @click.option('--lifetime', default=DEFAULT_LIFETIME, help='')
 @click.option('--fetch-hardware', default=False, help='')
 @click.option('--no-certificate-check', default=False, help='Disables TLS certificate check')
+@click.option('--step', default=15, help='Time step between each push, in seconds.')
 def prometheus_push(push_url, push_job, push_suffix,
     start_time, end_time, verbose, location, measure_power, lifetime,
-    fetch_hardware, no_certificate_check
+    fetch_hardware, no_certificate_check, step
 ):
-    metrics = get_metrics(
-        iso8601_or_timestamp_as_timestamp(start_time),
-        iso8601_or_timestamp_as_timestamp(end_time),
-        verbose,
-        location,
-        measure_power,
-        lifetime,
-        fetch_hardware,
-    )
     labels = {
         "hostname": uname().nodename
     }
-    body = format_prometheus_output(metrics, verbose, labels=labels)
-    body += format_prometheus_metric(
-        metric_name="boagent_hardware_lifetime",
-        metric_description="Hardware lifetime hypothesis for calculation. In Years.",
-        metric_type="GAUGE",
-        metric_value=lifetime,
-        labels=labels
-    )
     url = "{}/{}/job/{}/hostname/{}".format(push_url, push_suffix, push_job, "oden")
-    p = requests.post(url, data=body, verify=True if not no_certificate_check else False)
+    if step > 0:
+        signal.signal(signal.SIGINT, handler)
+        while [ True ]:
+            metrics = get_metrics(
+                iso8601_or_timestamp_as_timestamp(start_time),
+                iso8601_or_timestamp_as_timestamp(end_time),
+                verbose,
+                location,
+                measure_power,
+                lifetime,
+                fetch_hardware,
+            )
+            body = format_prometheus_output(metrics, verbose, labels=labels)
+            body += format_prometheus_metric(
+                metric_name="boagent_hardware_lifetime",
+                metric_description="Hardware lifetime hypothesis for calculation. In Years.",
+                metric_type="GAUGE",
+                metric_value=lifetime,
+                labels=labels
+            )
+            p = requests.post(url, data=body, verify=True if not no_certificate_check else False)
+            time.sleep(step)
+    else:
+        metrics = get_metrics(
+            iso8601_or_timestamp_as_timestamp(start_time),
+            iso8601_or_timestamp_as_timestamp(end_time),
+            verbose,
+            location,
+            measure_power,
+            lifetime,
+            fetch_hardware,
+        )
+        body = format_prometheus_output(metrics, verbose, labels=labels)
+        body += format_prometheus_metric(
+            metric_name="boagent_hardware_lifetime",
+            metric_description="Hardware lifetime hypothesis for calculation. In Years.",
+            metric_type="GAUGE",
+            metric_value=lifetime,
+            labels=labels
+        )
+        p = requests.post(url, data=body, verify=True if not no_certificate_check else False)
 
 if __name__ == '__main__':
     prometheus_push()
